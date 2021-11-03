da7499fc101b4347314c3ed88b289454190815e1
Fernando Calmet <fercalmet@gmail.com>
2021-11-03 10:43:53 -0500

    first commit

---
+++ Module.Catalog.Core/Abstractions/ICatalogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Module.Catalog.Core.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Module.Catalog.Core.Abstractions
+{
+    public interface ICatalogDbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
